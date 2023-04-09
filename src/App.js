5e16d20e0889285236643ff6f54f297e431f1141
jpedram <jpedram@amazon.com>
2023-04-08 23:55:05 -0400

    tabs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Team } from './models';
+import { BrowserRouter as Router, Route, Link, Outlet, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
 //import './App.css';
 import SoccerStandingsTable from './SoccerStandingsTable'
 import axios from "axios";
@@ -14,19 +18,61 @@ import Schedule from './Schedule';
 
 Amplify.configure(awsconfig);
 
-const App = () => {
+class App extends React.Component {
+  state = {
+    imageUrl: ''
+  };
+
+  async componentDidMount() {
+    const imageUrl = await Storage.get('soccer_2.jpg');
+    this.setState({ imageUrl });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar bg="primary" variant="dark" expand="lg" className="mb-4">
+            <Container>
+              <Navbar.Brand>Long Island Men's Soccer 2023</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbar-nav" />
+              <Navbar.Collapse id="navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/standings">
+                    Standings
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/schedule">
+                    Schedules
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home imageUrl={this.state.imageUrl} />} />
+              <Route path="/standings" element={<SoccerStandingsTable />} />
+              <Route path="/schedule" element={<Schedule />} />
+            </Routes>
+          </Container>
+        </div>
+      </Router>
+    );
+  }
+}
+
+function Home({ imageUrl }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Long Island Men's Soccer 2023</h1>
-      </header>
-      <main>
-        <SoccerStandingsTable />
-        <Schedule />
-      </main>
+    <div>
+      <h1 className="mb-3"></h1>
+      {imageUrl && <img src={imageUrl} alt="Home" className="img-fluid" />}
     </div>
   );
-};
+}
 
 //export default App;
 export default withAuthenticator(App);
